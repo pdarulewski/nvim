ac889e011f5ee68f7263f4e0eb6c897de21246e8
Pawel Darulewski <pawel.darulewski@gmail.com>
2023-08-24 18:23:20 +0200

    feat: move from dotfiles

---
+++ lua/plugins/init.lua
@@ -0,0 +1,23 @@
+return {
+
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  { "kdheepak/lazygit.nvim" },
+  { "mbbill/undotree" },
+  { "goolord/alpha-nvim" },
+  { "RRethy/vim-illuminate" },
+  { "windwp/nvim-autopairs" },
+  { "numToStr/Comment.nvim" },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+  },
+  { "EdenEast/nightfox.nvim" },
+  { "nordtheme/vim" },
+  { "AlexvZyl/nordic.nvim" },
+  { "rebelot/kanagawa.nvim" },
+}
