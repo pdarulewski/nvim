b8ce199dc8cfb9aad61b3b95a6550572da97d9cb
Pawel Darulewski <pawel.darulewski@gmail.com>
2023-09-04 17:27:10 +0200

    feat: fix binding

--- lua/plugins/lsp_signature.lua
+++ lua/plugins/lsp_signature.lua
@@ -1,21 +1,21 @@
 -- function signature help
 
 local M = {
-  "ray-x/lsp_signature.nvim",
-  event = "VeryLazy",
+	"ray-x/lsp_signature.nvim",
+	event = "VeryLazy",
 }
 
 M.config = function()
-  local ok, lsp_signature = pcall(require, "lsp_signature")
-  if not ok then
-    return
-  end
+	local ok, lsp_signature = pcall(require, "lsp_signature")
+	if not ok then
+		return
+	end
 
-  lsp_signature.setup()
+	lsp_signature.setup()
 
-  vim.keymap.set({ 'n' }, '<C-k>', function()
-    require('lsp_signature').toggle_float_win()
-  end, { silent = true, noremap = true, desc = 'toggle signature' })
+	vim.keymap.set({ "n" }, "<C-Space>", function()
+		require("lsp_signature").toggle_float_win()
+	end, { silent = true, noremap = true, desc = "toggle signature" })
 end
 
 return M
