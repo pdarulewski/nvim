dc286c968bb9c3ef85dab607227671b542aa572f
Pawel Darulewski <pawel.darulewski@gmail.com>
2023-11-08 17:47:15 +0100

    feat: python venv selector

---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,19 @@
+-- venv
+local M = {
+  "linux-cultist/venv-selector.nvim",
+  event = "VeryLazy",
+}
+
+M.config = function()
+  local ok, venv = pcall(require, "venv-selector")
+  if not ok then
+    return
+  end
+
+  venv.setup({
+    name = ".venv",
+    parents = 1,
+  })
+end
+
+return M
