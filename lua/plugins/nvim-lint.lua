64a77458a1c6652c214824707f7a3e9442e8f714
Pawel Darulewski <pawel.darulewski@gmail.com>
2025-03-29 14:02:30 +0100

    feat: golangci-lint v2

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -34,5 +34,7 @@ M.config = function()
 
 	lint.linters.markdownlint.args = { "--config", "/Users/pd/.config/markdownlint/markdownlint.json", "--" }
 
+	lint.linters.golangcilint.args =
+		{ "run", "--output.json.path=stdout", "--issues-exit-code=0", "--show-stats=false" }
 end
 return M
